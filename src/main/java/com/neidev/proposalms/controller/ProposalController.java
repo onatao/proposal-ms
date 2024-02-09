aa32dc1cc153af63e4268a09a0ab83d3dbacdefb
Nathan <nathanbarrosnt@gmail.com>
2024-02-09 10:14:07 -0300

    feat: service structure

--- src/main/java/com/neidev/proposalms/controller/ProposalController.java
+++ src/main/java/com/neidev/proposalms/controller/ProposalController.java
@@ -3,6 +3,7 @@ package com.neidev.proposalms.controller;
 import com.neidev.proposalms.entity.Proposal;
 import com.neidev.proposalms.json.ProposalRequestForm;
 import com.neidev.proposalms.json.ProposalResponseForm;
+import com.neidev.proposalms.service.ProposalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposal")
 public class ProposalController {
 
+    private final ProposalService proposalService;
+
+    public ProposalController(ProposalService proposalService) {
+        this.proposalService = proposalService;
+    }
+
     @PostMapping()
     public ResponseEntity<ProposalResponseForm> create(@RequestBody ProposalRequestForm data) {
-        return  null;
+        var response = proposalService.create(data);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/neidev/proposalms/service/ProposalService.java
@@ -0,0 +1,13 @@
+package com.neidev.proposalms.service;
+
+import com.neidev.proposalms.json.ProposalRequestForm;
+import com.neidev.proposalms.json.ProposalResponseForm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProposalService {
+
+    public ProposalResponseForm create(ProposalRequestForm data) {
+        return null;
+    }
+}
