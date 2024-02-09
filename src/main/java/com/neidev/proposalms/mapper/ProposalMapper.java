7a3af6ab5a8b28706a4b9aa51dc5be4c9ba7208d
Nathan <nathanbarrosnt@gmail.com>
2024-02-09 12:45:10 -0300

    feat: dto mapping finish & service impl

--- src/main/java/com/neidev/proposalms/mapper/ProposalMapper.java
+++ src/main/java/com/neidev/proposalms/mapper/ProposalMapper.java
@@ -2,11 +2,15 @@ package com.neidev.proposalms.mapper;
 
 import com.neidev.proposalms.entity.Proposal;
 import com.neidev.proposalms.json.ProposalRequestForm;
+import com.neidev.proposalms.json.ProposalResponseForm;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ProposalMapper {
+
+    ProposalMapper INSTANCE = Mappers.getMapper(ProposalMapper.class);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "approved", ignore = true)
@@ -17,5 +21,12 @@ public interface ProposalMapper {
     @Mapping(target = "user.cpf", source = "cpf")
     @Mapping(target = "user.phoneNumber", source = "phoneNumber")
     @Mapping(target = "user.income", source = "income")
-    Proposal toProposal(ProposalRequestForm data);
+    Proposal toEntity(ProposalRequestForm data);
+
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "lastName", source = "user.lastName")
+    @Mapping(target = "phoneNumber", source = "user.phoneNumber")
+    @Mapping(target = "cpf", source = "user.cpf")
+    @Mapping(target = "income", source = "user.income")
+    ProposalResponseForm toResponse(Proposal data);
 }
--- src/main/java/com/neidev/proposalms/service/ProposalService.java
+++ src/main/java/com/neidev/proposalms/service/ProposalService.java
@@ -2,9 +2,11 @@ package com.neidev.proposalms.service;
 
 import com.neidev.proposalms.json.ProposalRequestForm;
 import com.neidev.proposalms.json.ProposalResponseForm;
+import com.neidev.proposalms.mapper.ProposalMapper;
 import com.neidev.proposalms.repository.ProposalRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -12,7 +14,10 @@ public class ProposalService {
 
     private final ProposalRepository proposalRepository;
 
+    @Transactional
     public ProposalResponseForm create(ProposalRequestForm data) {
-        return null;
+        var entity = ProposalMapper.INSTANCE.toEntity(data);
+        proposalRepository.save(entity);
+        return ProposalMapper.INSTANCE.toResponse(entity);
     }
 }
