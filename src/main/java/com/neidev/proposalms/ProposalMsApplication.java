4b85669880240659eed8b1837aedec928bd01d56
Nathan <nathanbarrosnt@gmail.com>
2024-02-09 09:05:59 -0300

    release: structure

---
+++ src/main/java/com/neidev/proposalms/ProposalMsApplication.java
@@ -0,0 +1,13 @@
+package com.neidev.proposalms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neidev/proposalms/ProposalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neidev.proposalms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
