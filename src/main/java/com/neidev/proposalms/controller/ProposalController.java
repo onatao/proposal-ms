c95f17c1bb182cc90cd1e4d50974f29a5399afdd
Nathan <nathanbarrosnt@gmail.com>
2024-02-11 10:55:39 -0300

    fix: refactored controller - returning uri location

--- src/main/java/com/neidev/proposalms/controller/ProposalController.java
+++ src/main/java/com/neidev/proposalms/controller/ProposalController.java
@@ -4,11 +4,13 @@ import com.neidev.proposalms.json.ProposalRequestForm;
 import com.neidev.proposalms.json.ProposalResponseForm;
 import com.neidev.proposalms.service.ProposalService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/proposal")
@@ -20,6 +22,8 @@ public class ProposalController {
     @PostMapping()
     public ResponseEntity<ProposalResponseForm> create(@RequestBody ProposalRequestForm data) {
         var response = proposalService.create(data);
-        return ResponseEntity.ok(response);
+        // returning status code 201 & resource path, location.
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(response.getId()).toUri()).body(response);
     }
 }
