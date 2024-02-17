0f65ed7a17804a7799230fe8366bd18a02ffe461
Nathan <nathanbarrosnt@gmail.com>
2024-02-17 14:48:06 -0300

    fix: overwriting RabbitTemplate bean implementation - serialization fixed

--- src/main/java/com/neidev/proposalms/config/RabbitMqConfiguration.java
+++ src/main/java/com/neidev/proposalms/config/RabbitMqConfiguration.java
@@ -1,8 +1,12 @@
 package com.neidev.proposalms.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +14,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfiguration {
+
+    @Bean
+    public MessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
 
     // Consumed by credit microservice
     @Bean
